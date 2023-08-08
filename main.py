a67d029a7a469b222e6c6956f1cdf7c54115d9fb
Rafael Silva <todosomtodavoz@gmail.com>
2023-08-08 08:43:36 -0300

    Utilizando o padrão FActory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from Cpf import Cpf
+from cpf_cnpj import Documento
 
-cpf = "87458965285"
-objeto_cpf = Cpf(cpf)
-print(objeto_cpf)
+exemplo_cnpj = "09984950000172"
+exemplo_cpf = "38774596845"
 
+documento = Documento.cria_documento(exemplo_cnpj)
+print(documento)
