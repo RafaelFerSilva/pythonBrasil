8194e8f57d40da5d81066a5908eeec229377fe8d
Rafael Silva <todosomtodavoz@gmail.com>
2023-08-13 11:43:20 -0300

    Acessar API e capturar dados pelo CEP

--- acesso_cep.py
+++ acesso_cep.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class BuscaEndereco:
 
     def __init__(self, cep):
@@ -17,4 +20,14 @@ class BuscaEndereco:
             return False
 
     def format_cep(self):
-        return "{}-{}".format(self.cep[:5], self.cep[4:])
+        return "{}-{}".format(self.cep[:5], self.cep[5:])
+
+    def acessa_via_cep(self):
+        url = "https://viacep.com.br/ws/{}/json/".format(self.cep)
+        r = requests.get(url)
+        dados = r.json()
+        return (
+            dados['bairro'],
+            dados['localidade'],
+            dados['uf']
+        )
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from acesso_cep import BuscaEndereco
+import requests
 
-cep = 25870145
+cep = 13803096
 objeto_cep = BuscaEndereco(cep)
 print(objeto_cep)
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro)
 
